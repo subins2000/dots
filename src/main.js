4fac210fe4ccaf8e0c7c0bac4d3edde78b7fd7fd
Subin Siby <mail@subinsb.com>
2020-05-03 01:15:04 +0530

    Add bulma

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
