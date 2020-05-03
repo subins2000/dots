ced2748177192058a80a0da354543d28338fc46f
Athul Cyriac Ajay <athul8720@gmail.com>
2020-05-03 16:20:12 +0530

    Small vue changes

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,113 +1,50 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+  <div>
+    <div class="container has-text-centered">
+    <h1 class="title is-1">{{ msg }}</h1>
+   <input type="text" class="input" name="name" v-model="gname" placeholder="Paste the Game Name here and Press Enter" @keyup.enter="join" /><br/><br/>
+   <strong class="is-primary has-text-centered">OR</strong><br/>
+    </div><br/>
+   <div class="has-text-centered">
+      <button class="button is-medium is-primary is-center" @click="nwgame">START New Game</button>
+    </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Index',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      gname: ''
+    }
+  },
+  methods:{
+    nwgame () {
+      this.$router.push('register')
+      localStorage.setItem('gameName',0)
+    },
+    join(){
+      if (this.gname===''){
+        alert("Please Enter Valid Game Name")
+        this.$router.push('')
+      }
+      localStorage.setItem('gameName',this.gname)
+      this.$router.push('register')
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.container {
+  padding-top: 10vh;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
+button {
+  margin-top: 4px;
+  margin: 0 auto;
   display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
