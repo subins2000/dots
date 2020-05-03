999fc27dad19cbc4f96e3a8826f95482e36c0d93
Subin Siby <mail@subinsb.com>
2020-05-03 15:22:54 +0530

    Implement box completed logic and colors

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -51,30 +51,177 @@ export default {
             .attr('height', cellWidth - cellMargin)
             .attr('x', cellMargin - 3)
             .attr('y', cellMargin - 3)
+            .attr('id', i + '-' + j)
           
           // Add lines
           cell.append('line')
-            .attr('class', 'vline')
+            .attr('class', 'line vline')
             .attr('y1', cellMargin)
             .attr('y2', cellWidth - cellMargin)
             .attr('x1', 0)
             .attr('x2', 0)
             .attr('stroke-width', cellMargin)
             .attr('stroke-linecap', 'round')
+            .attr('id', i + '-' + j)
           
           cell.append('line')
-            .attr('class', 'hline')
+            .attr('class', 'line hline')
             .attr('x1', cellMargin)
             .attr('x2', cellWidth - cellMargin)
             .attr('y1', 0)
             .attr('y2', 0)
             .attr('stroke-width', cellMargin)
             .attr('stroke-linecap', 'round')
+            .attr('id', i + '-' + j)
+                 
+          // Add horizontal line below last row elems
+          if (i == gridSize - 1) {
+            cell.append('line')
+              .attr('class', 'line hline')
+              .attr('x1', cellMargin)
+              .attr('x2', cellWidth - cellMargin)
+              .attr('y1', cellWidth)
+              .attr('y2', cellWidth)
+              .attr('stroke-width', cellMargin)
+              .attr('stroke-linecap', 'round')
+              .attr('id', (i + 1) + '-' + j)
+          }
+
+          // Add vertical line on right of last column elems
+          if (j == gridSize - 1) {
+            cell.append('line')
+              .attr('class', 'line vline')
+              .attr('y1', cellMargin)
+              .attr('y2', cellWidth - cellMargin)
+              .attr('x1', cellWidth)
+              .attr('x2', cellWidth)
+              .attr('stroke-width', cellMargin)
+              .attr('stroke-linecap', 'round')
+              .attr('id', i + '-' + (j + 1))
+          }
         }
       }
+
+      // Add events
+      this.game.addEventListener('click', this.onClick)
+    },
+    onClick (e) {
+      var elem = e.target
+
+      if (elem.classList.contains('line')) {
+        // It's a line
+        elem.classList.add('active')
+        var completedBoxes = this.boxComplete(elem)
+        var box
+        for (var id in completedBoxes) {
+          box = completedBoxes[id]
+          if (box) {
+            box = this.game.querySelector('.cell[id="' + id + '"]')
+            box.classList.add('active')
+          }
+        }
+      }
+    },
+    boxComplete (activeLine) {
+      // Will have the lines to check if active to know which boxes got filled
+      var lines = []
+
+      // The boxes being considered to be filled
+      // If activeLine is vertical, then consider boxes left & right of the line
+      // If activeLine is horizontal, then consider boxes top & down of the line
+      var consideringBoxes = []
+      
+      var [row, col] = activeLine.id.split('-')
+      row = parseInt(row)
+      col = parseInt(col)
+      
+      // If horizontal line is chosen
+      if (activeLine.classList.contains('hline')) {
+        if (row > 0) {
+          lines.push(
+            this.game.querySelector('.vline[id="' + (row - 1) + '-' + col + '"]')
+          )
+          lines.push(
+            this.game.querySelector('.hline[id="' + (row - 1) + '-' + col + '"]')
+          )
+          
+          if (col + 1 <= gridSize) {
+            lines.push(
+              this.game.querySelector('.vline[id="' + ((row - 1) + '-' + (col + 1)) + '"]')
+            )
+          }
+
+          consideringBoxes[((row - 1) + '-' + col)] = false
+        }
+
+        if (row + 1 <= gridSize) {
+          lines.push(
+            this.game.querySelector('.vline[id="' + (row + '-' + col) + '"]')
+          )
+          lines.push(
+            this.game.querySelector('.hline[id="' + ((row + 1) + '-' + col) + '"]')
+          ) // Cause row'th one is the activeLine
+
+          if (col + 1 <= gridSize) {
+            lines.push(
+              this.game.querySelector('.vline[id="' + (row + '-' + (col + 1)) + '"]')
+            )
+          }
+
+          consideringBoxes[(row + '-' + col)] = false
+        }
+      }
+
+      // If vertical line is chosen
+      if (activeLine.classList.contains('vline')) {
+        if (col > 0) {
+          lines.push(
+            this.game.querySelector('.vline[id="' + row + '-' + (col - 1) + '"]')
+          )
+          lines.push(
+            this.game.querySelector('.hline[id="' + row + '-' + (col - 1) + '"]')
+          )
+          
+          if (row + 1 <= gridSize) {
+            lines.push(
+              this.game.querySelector('.hline[id="' + ((row + 1) + '-' + (col - 1)) + '"]')
+            )
+          }
+
+          consideringBoxes[(row + '-' + (col - 1))] = false
+        }
+
+        if (col + 1 <= gridSize) {
+          lines.push(
+            this.game.querySelector('.vline[id="' + (row + '-' + (col + 1)) + '"]')
+          ) // Cause col'th one is the activeLine
+          lines.push(
+            this.game.querySelector('.hline[id="' + (row + '-' + col) + '"]')
+          )
+
+          if (row + 1 <= gridSize) {
+            lines.push(
+              this.game.querySelector('.hline[id="' + ((row + 1) + '-' + col) + '"]')
+            )
+          }
+
+          consideringBoxes[(row + '-' + col)] = false
+        }
+      }
+
+      // Is first box complete ?
+      consideringBoxes[Object.keys(consideringBoxes)[0]] = lines[0].classList.contains('active') && lines[1].classList.contains('active') && lines[2].classList.contains('active')
+
+      // Is second box complete ?
+      if (lines[3]) {
+        consideringBoxes[Object.keys(consideringBoxes)[1]] = lines[3].classList.contains('active') && lines[4].classList.contains('active') && lines[5].classList.contains('active')
+      }
+
+      return consideringBoxes
     }
   },
   mounted () {
+    this.game = this.$refs.game
     this.svg = d3.select(this.$refs.game)
     this.makeGameBoard()
   }
@@ -87,8 +234,16 @@ export default {
   fill: #EEE
 }
 
-#game .hline, #game .vline {
+#game .cell.active {
+  fill: #ffa7d3
+}
+
+#game .line {
   stroke: #BBB
+}
+
+#game .line.active {
+  stroke: #CF649A
 }
 
 #game-wrapper {
