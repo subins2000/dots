a2f4d8c95bbecd70a3f43d09ffe7a6093aec4ff2
Subin Siby <mail@subinsb.com>
2020-05-09 13:09:24 +0530

    Add a game history index for better sync

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -155,9 +155,11 @@ export default {
 
       /**
        * History of all line markings
+       * Index: a sequential ID
        * Value: [playerID, lineType, lineID]
        */
-      gameHistory: [],
+      gameHistory: {},
+      gameHistoryIndex: -1,
 
       /**
        * History of all line markings that made a box completion
@@ -224,7 +226,7 @@ export default {
           playerID: $this.myID,
           name: $this.myName,
           colors: $this.players[$this.myID].colors,
-          historyLength: $this.gameHistory.length,
+          historyIndex: $this.gameHistoryIndex,
           playerCount: Object.keys(this.players).length
         }))
 
@@ -268,7 +270,7 @@ export default {
 
           $this.playerTurns[msg.playerID] = false
 
-          if (msg.historyLength > $this.gameHistory.length) {
+          if (msg.historyIndex > $this.gameHistoryIndex) {
             // My game history is not latest
             // I probably joined the game in between
             $this.expectingPlayerCount = Math.max($this.expectingPlayerCount, msg.playerCount)
@@ -480,14 +482,24 @@ export default {
       this.activateLine(elem, this.myID)
     },
 
-    activateLine (line, playerID, playAudio = true) {
+    activateLine (line, playerID, playAudio = true, historyIndex = false) {
+      if (historyIndex && this.gameHistory[historyIndex]) {
+        return
+      }
+
       var audioToPlay = 'mark'
 
       line.classList.add('active')
       line.style.stroke = this.players[playerID].colors[0]
 
       var lineType = line.classList.contains('hline') ? 'h' : 'v'
-      this.gameHistory.push([playerID, lineType, line.id])
+
+      if (historyIndex) {
+        this.gameHistory[historyIndex] = [playerID, lineType, line.id]
+        this.gameHistoryIndex = historyIndex
+      } else {
+        this.gameHistory[++this.gameHistoryIndex] = [playerID, lineType, line.id]
+      }
 
       var completedBoxes = this.boxComplete(line)
       var box
@@ -703,10 +715,10 @@ export default {
 
     restoreGameState (history) {
       var h
-      for (var hi in history) {
-        h = history[hi] // [playerID, lineType, lineID]
+      for (var historyIndex in history) {
+        h = history[historyIndex] // [playerID, lineType, lineID]
 
-        this.activateLine(this.game.querySelector('.' + h[1] + 'line[id="' + h[2] + '"]'), h[0], false)
+        this.activateLine(this.game.querySelector('.' + h[1] + 'line[id="' + h[2] + '"]'), h[0], false, historyIndex)
       }
     },
 
@@ -766,11 +778,11 @@ export default {
 
       var nextPlayerID;
 
-      if (this.gameHistory.length === 0) {
+      if (this.gameHistoryIndex === -1) {
         // First player in list has the turn
         nextPlayerID = this.playerTurns.indexOf(false)
       } else {
-        var lastPlay = this.gameHistory[this.gameHistory.length - 1]
+        var lastPlay = this.gameHistory[this.gameHistoryIndex]
         var lastPlayerID = parseInt(lastPlay[0])
         var lastPlayedLine = lastPlay[1] + lastPlay[2]
 
