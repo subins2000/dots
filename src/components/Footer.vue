6d9a9851e7abfc33fa875384c0e272cd555c3be4
Subin Siby <mail@subinsb.com>
2020-05-10 12:06:04 +0530

    Less footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,16 +10,13 @@
       </div>
       <div class='level-right'>
         <div class='level-item'>
-          Copyleft {{ new Date().getFullYear() }} |
+          Copyleft {{ new Date().getFullYear() }}
         </div>
         <div class='level-item'>
-          Licensed under <a href='https://www.gnu.org/licenses/agpl-3.0.en.html'>AGPLv3</a>&nbsp;|
-        </div>
-        <div class='level-item'>
-          <a href='https://github.com/subins2000/vett'>Source Code </a>&nbsp;|
+          <a href='https://github.com/subins2000/vett'>Source Code </a>&nbsp;
         </div>
          <div class='level-item'>
-          Made by&nbsp;<a href='https://github.com/subins2000'>&nbsp;Subin Siby </a>&nbsp;and&nbsp;<a href='https://github.com/athul'>Athul Cyriac Ajay&nbsp;</a>
+          Made by&nbsp;<a href='https://subinsb.com/'>&nbsp;Subin Siby </a>&nbsp;and&nbsp;<a href='https://github.com/athul'>Athul Cyriac Ajay&nbsp;</a>
         </div>
       </div>
     </nav>
