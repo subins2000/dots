69382c1eef4cdf114f86bfcbdcbbce9d7070d16e
Subin Siby <mail@subinsb.com>
2020-08-14 03:23:05 +0530

    Add new tracker

--- src/main.js
+++ src/main.js
@@ -62,6 +62,7 @@ Vue.prototype.isGameCodeValid = (gameCode) => {
 }
 
 var announceURLs = [
+  'wss://wsswt.herokuapp.com/',
   'wss://tracker.openwebtorrent.com',
   'wss://tracker.sloppyta.co:443/announce',
   'wss://tracker.novage.com.ua:443/announce',
